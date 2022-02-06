8ad4ca49afe5d1d3792300de454186578e377b4f
Crowdbotics <team@crowdbotics.com>
2022-02-06 16:13:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "leech_lake_gaming_33400",
+  url: "https://leech_lake_gaming_33400.botics.co",
+  api: "https://leech_lake_gaming_33400.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
